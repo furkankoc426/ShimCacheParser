0f4c62048d9318cdd6a0a15c19baf909688a8dfe
KOC <KOC@DESKTOP-L58H131>
2022-03-30 21:24:03 +0300

    Added Vista.

--- ShimCacheParser.c
+++ ShimCacheParser.c
@@ -25,10 +25,13 @@ const DWORD WIN8_FILETIME_OFFSET = 0x18;
 const DWORD WIN8_0_MAGIC_NUMBER = 0x73743030;
 const DWORD WIN8_1_MAGIC_NUMBER = 0x73743031;
 
+// COMMON WIN7
 const DWORD WIN7_HEADER_SIZE = 0x80;
 const DWORD WIN7_MAGIC_NUMBER = 0xBADC0FEE;
 const DWORD WIN7_ENTRY_COUNT_OFFSET = 0x04;
 const DWORD WIN7_FILE_OFFSET = 0x08;
+
+// SPECIAL 
 const DWORD WIN7_32_ENTRY_SIZE = 0x20;
 const DWORD WIN7_64_ENTRY_SIZE = 0x30;
 
@@ -57,8 +60,36 @@ struct WIN7_64_Entry
 	QWORD blobOffset;
 };
 
+// COMMON WINVISTA
 const DWORD WINVISTA_HEADER_SIZE = 0x08;
 const DWORD WINVISTA_MAGIC_NUMBER = 0xBADC0FFE;
+const DWORD WINVISTA_ENTRY_COUNT_OFFSET = 0x04;
+const DWORD WINVISTA_FILE_OFFSET = 0x08;
+
+// SPECIAL 
+const DWORD WINVISTA_32_ENTRY_SIZE = 0x18;
+const DWORD WINVISTA_64_ENTRY_SIZE = 0x20;
+
+struct WINVISTA_32_Entry
+{
+    USHORT pathLength;
+	USHORT pathMaxLength;
+	DWORD pathOffset;
+	FILETIME lastModTime;
+	DWORD insertFlags;
+	DWORD shimFlags;
+};
+
+struct WINVISTA_64_Entry
+{
+    USHORT pathLength;
+	USHORT pathMaxLength;
+	DWORD padding;
+	QWORD pathOffset;
+	FILETIME lastModTime;
+	DWORD insertFlags;
+	DWORD shimFlags;
+};
 
 const DWORD WIN2K3_32_FILESIZE_OFFSET = 0x10;
 const DWORD WIN2K3_64_FILESIZE_OFFSET = 0x18;
@@ -70,24 +101,48 @@ BOOL parseWinXP(PUCHAR dataBuffer, DWORD dataSize)
 
 }
 
-BOOL parseWin2K3_32(PUCHAR dataBuffer, DWORD dataSize)
+BOOL parseWinVista(PUCHAR dataBuffer, DWORD dataSize, const char* fileName, BOOL is32bit)
 {
+    FILE * file = fopen(fileName, "w");
+    fprintf(file, "FilePath;LastModifiedTime\n");
+    
+    PUCHAR pathOffset;
+    USHORT pathLength; 
+    FILETIME lastModTime;
+    DWORD entrySize = (is32bit ? WINVISTA_32_ENTRY_SIZE : WINVISTA_64_ENTRY_SIZE );
 
-}
+    CHAR filePath[512];
+    UINT entryCount = *(UINT*)(dataBuffer + WINVISTA_ENTRY_COUNT_OFFSET);
+    PUCHAR index = dataBuffer + WINVISTA_HEADER_SIZE;
+    printf("entryCount: %d\n", entryCount);
+    UINT counter = 0;
+    while( index < dataBuffer + dataSize && counter < entryCount) {
+        if(is32bit)
+        {
+            struct WINVISTA_32_Entry entry = *(struct WINVISTA_32_Entry*)index;
+            lastModTime = entry.lastModTime;
+            pathLength = entry.pathLength - WINVISTA_FILE_OFFSET;
+            pathOffset = dataBuffer + WINVISTA_FILE_OFFSET + entry.pathOffset;
+        }
+        else
+        {
+            struct WINVISTA_64_Entry entry = *(struct WINVISTA_64_Entry*)index;
+            lastModTime = entry.lastModTime;
+            pathLength = entry.pathLength - WINVISTA_FILE_OFFSET;
+            pathOffset = dataBuffer + WINVISTA_FILE_OFFSET + entry.pathOffset;
+        }
 
-BOOL parseWin2K3_64(PUCHAR dataBuffer, DWORD dataSize)
-{
+        wcstombs(filePath, (const wchar_t *)pathOffset, pathLength);
+        SYSTEMTIME st;    
+        FileTimeToSystemTime(&lastModTime, &st);
+        fwrite(filePath, pathLength/2, 1, file);
+        fprintf(file, ";%d/%02d/%02d-%02d:%02d:%02d\n", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
 
-}
-
-BOOL parseWinVista_32(PUCHAR dataBuffer, DWORD dataSize)
-{
-
-}
-
-BOOL parseWinVista_64(PUCHAR dataBuffer, DWORD dataSize)
-{
-
+        index += entrySize;        
+        counter++;
+    }
+    fclose(file);
+    return TRUE;
 }
 
 BOOL parseWin7(PUCHAR dataBuffer, DWORD dataSize, const char* fileName, BOOL is32bit)
@@ -130,6 +185,8 @@ BOOL parseWin7(PUCHAR dataBuffer, DWORD dataSize, const char* fileName, BOOL is3
         index += entrySize;        
         counter++;
     }
+    fclose(file);
+    return TRUE;
 }
 
 BOOL parseWin8(PUCHAR dataBuffer, DWORD dataSize, const char* fileName, const DWORD magicNumber)
@@ -198,29 +255,33 @@ enum WINDOWS_VERSION checkVersion(PUCHAR dataBuffer, DWORD dataSize)
     if(WINVISTA_MAGIC_NUMBER == *(UINT32*)dataBuffer)
     {
         UINT32 fileSizeLow;
-        fileSizeLow = *(UINT32*)(dataBuffer + WINVISTA_HEADER_SIZE + WIN2K3_32_FILESIZE_OFFSET);
-        if(fileSizeLow > 3)
-        {
-            return WIN_2K3_32;
-        }
-
-        fileSizeLow = *(UINT32*)(dataBuffer + WINVISTA_HEADER_SIZE + WIN2K3_64_FILESIZE_OFFSET);
-        if(fileSizeLow > 3)
-        {
-            return WIN_2K3_64;
-        }
-
         UINT16 length = *(UINT16*)(dataBuffer + WINVISTA_HEADER_SIZE);
         UINT16 maxlength = *(UINT16*)(dataBuffer + WINVISTA_HEADER_SIZE + 2);
         UINT32 padding = *(UINT32*)(dataBuffer + WINVISTA_HEADER_SIZE + 4);
         if (maxlength - length == 0x02) {
             if (padding == 0x00000000)
             {
-                return WIN_VISTA_64;
+                fileSizeLow = *(UINT32*)(dataBuffer + WINVISTA_HEADER_SIZE + WIN2K3_64_FILESIZE_OFFSET);
+                if(fileSizeLow > 3)
+                {
+                    return WIN_2K3_64;
+                }
+                else
+                {
+                    return WIN_VISTA_64;
+                }
             }
             else
             {
-                return WIN_VISTA_32;
+                fileSizeLow = *(UINT32*)(dataBuffer + WINVISTA_HEADER_SIZE + WIN2K3_32_FILESIZE_OFFSET);
+                if(fileSizeLow > 3)
+                {
+                    return WIN_2K3_32;
+                }
+                else
+                {
+                    return WIN_VISTA_32;
+                }
             }
         }    
     }
@@ -277,19 +338,19 @@ BOOL parse(PUCHAR dataBuffer, DWORD dataSize)
         break;
     case WIN_2K3_32:
         printf("WIN_2K3_32 parser\n");
-        ret = parseWin2K3_32(dataBuffer, dataSize);
+        ret = parseWinVista(dataBuffer, dataSize, "AppCompatCache_Win_2k3_32.txt", TRUE);
         break;
     case WIN_2K3_64:
         printf("WIN_2K3_64 parser\n");
-        ret = parseWin2K3_64(dataBuffer, dataSize);
+        ret = parseWinVista(dataBuffer, dataSize, "AppCompatCache_Win_2k3_XP_64.txt", FALSE);
         break;
     case WIN_VISTA_32:
         printf("WIN_VISTA_32 parser\n");
-        ret = parseWinVista_32(dataBuffer, dataSize);
+        ret = parseWinVista(dataBuffer, dataSize, "AppCompatCache_Win_Vista_2k8_32.txt", TRUE);
         break;
     case WIN_VISTA_64:
         printf("WIN_VISTA_64 parser\n");
-        ret = parseWinVista_64(dataBuffer, dataSize);
+        ret = parseWinVista(dataBuffer, dataSize, "AppCompatCache_Win_Vista_2k8_64.txt", FALSE);
         break;
     case WIN_7_32:
         printf("WIN_7_32 parser\n");
